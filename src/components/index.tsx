eed6d0565e218e94d102e31f4dd3ee6d58264977
wangyhu <wangyuhu@sensoro.com>
2023-06-13 16:55:50 +0800

    feat: 组件统一导出

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as Canvas } from "./Canvas";
+export { default as LoadingModal } from "./LoadingModal";
+export { default as SegmentDrawer } from "./SegmentDrawer";
+export { default as SegmentOptions } from "./SegmentOptions";
+export { default as SvgMask } from "./SvgMask";
