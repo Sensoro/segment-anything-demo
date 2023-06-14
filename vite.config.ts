17dcef60c3b1e3bd56180b5949f769b840f026cb
wangxingkang <wang_xingkang@qq.com>
2023-06-14 13:39:58 +0800

    chore:  onnxruntime-web replace to cdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // base: '/segment-anything/',
+  plugins: [
+    react(),
+    viteExternalsPlugin({
+      'onnxruntime-web': 'ort'
+    })
+  ],
   define: {
     'process.env': {},
   },
