da05b5dc8f8a2d3b7d98a202b74983fbe02f3c22
wangxingkang <wang_xingkang@qq.com>
2023-06-14 11:10:41 +0800

    ci: add deploy ci

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": {},
+    'process.env': {},
+  },
+  server: {
+    open: true,
   },
 });
